6ed1282991150b9066077a58f8418a2d0efb206e
Marián Gloser <147488596+mariangloser@users.noreply.github.com>
2023-10-13 02:20:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting){
-            entry.target.classList.add('show');
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show'); // Show the section
+
+            // Remove the blur filter when the section is visible
+            entry.target.style.filter = 'none';
         } else {
-            entry.target.classList.remove('show');
+            entry.target.classList.remove('show'); // Hide the section
+
+            // Add the blur filter when the section is hidden
+            entry.target.style.filter = 'blur(5px)';
         }
     });
 });
