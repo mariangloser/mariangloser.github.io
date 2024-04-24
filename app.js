68a0b791d25c4a106077c08d5bbadd03c0dc3254
Marián Gloser <147488596+mariangloser@users.noreply.github.com>
2024-04-25 00:16:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -16,3 +16,15 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+// Making the age variable
+const birthday = new Date(2002,0,10);
+const today = new Date();
+
+if (today.getMonth() >= birthday.getMonth() && today.getDay() >= birthday.getDay()){
+    document.getElementById("birthday").innerHTML = today.getFullYear() - birthday.getFullYear();
+}
+
+else {
+    document.getElementById("birthday").innerHTML = today.getFullYear() - birthday.getFullYear() - 1;
+}
